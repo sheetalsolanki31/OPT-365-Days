56fdc9a2b4602b2c90536c9d3d11716036dd7128
sheetalsolanki31 <solankisheetal234@gmail.com>
2026-07-23 00:16:11 +0530

    operators learned

--- Day-01/day01.py
+++ Day-01/day01.py
@@ -1,5 +1,11 @@
+
+#print function
+
 print("Hii sheetal")
 print("I started learning python today")
+
+
+#variables and data types
 
 name = "sheetal"
 age = 24
@@ -18,7 +24,29 @@ print(type(age))
 print(type(height))
 print(type(is_learning))
 
+#user input
 
 
 name = input("enter your name")
 print("Hello", name)
+
+
+#input is always string so we need to convert it into integer
+
+age = int(input("enter your age: "))
+print("You are", age, "years old")
+print(type(age))
+
+
+#basic arithmetic operations
+
+a = 10 
+b = 5 
+
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a//b)
+print(a%b)
+print(a**b)
