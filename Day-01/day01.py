06967fb9188ca7b96462e44ece86713a09144675
sheetalsolanki31 <solankisheetal234@gmail.com>
2026-07-23 00:02:44 +0530

    input function checked

--- Day-01/day01.py
+++ Day-01/day01.py
@@ -1,2 +1,24 @@
 print("Hii sheetal")
 print("I started learning python today")
+
+name = "sheetal"
+age = 24
+height = 5.2
+is_learning = True
+
+
+print(name)
+print(age)
+print(height)
+print(is_learning)
+
+
+print(type(name) )
+print(type(age))
+print(type(height))
+print(type(is_learning))
+
+
+
+name = input("enter your name")
+print("Hello", name)
